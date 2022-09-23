f64c055de511be992e277bd3afbd9dc4f687b6d2
Nikita <78297763+AdolfMuffin@users.noreply.github.com>
2022-09-23 11:31:04 +0300

    Update clear_elk_indicies.sh

--- clear_elk_indicies.sh
+++ clear_elk_indicies.sh
@@ -1,2 +1,3 @@
-#Command finds unused indicies on elk host and removes them
-while true ; do i=$(curl http://$elk_host:9200/* | jq . | grep logs | sed -n 'p;n' | cut -d '"' -f2 | head -1) ; curl -XDELETE http://$elk_host:9200/$i ; done
+#Однострочник находит и удаляет неиспользуемые индексы ELK
+#Запуская скрипт, необходимо указать URL сервиса ELK
+while true ; do i=$(curl '$1':9200/* | jq . | grep logs | sed -n 'p;n' | cut -d '"' -f2 | head -1) ; curl -XDELETE '$1':9200/$i ; done
