f90924176d5275016b2dd32ba602c155cfe30913
Nikita Ivanov <n.ivanov@bpcbt.com>
2022-11-09 16:56:34 +0300

    Оптимизация скрипта add_host.sh

--- zabbix/README.md
+++ zabbix/README.md
@@ -1,9 +1,8 @@
 # Наработки по Zabbix
 ## add_host.sh
 Скрипту необходимо предоставить файл list.csv в формате <hostname>,<ip address> и разместить его в той же директории.
-Кроме того, перед запуском скрипта нужно отредактировать переменную ZABBIX_URL.
 При запуске скрипта необходимо указать следующие ключи:
-./add_host.sh <username> <password> <host_group>
+./add_host.sh <username> <password> <host_group> <zabbix_url>
 Пользователь должен иметь достаточно прав в Zabbix для добавления хостов в выбранную группу.
 
 ## domain_expiration_check
--- zabbix/add_host.sh
+++ zabbix/add_host.sh
@@ -2,13 +2,11 @@
 
 i=0
 
-ZABBIX_URL=<PASTE ZABBIX URL HERE>
-
 HOST_COUNT=$(wc -l ./list.csv | cut -d" " -f1)
 
-API_TOKEN=$(curl -s -k -X POST -H 'Content-Type: application/json-rpc' -d '{"jsonrpc":"2.0", "method":"user.login", "params": {"user": "'$1'", "password": "'$2'"}, "auth": null, "id": 0}' ''$ZABBIX_URL'/api_jsonrpc.php' | jq -r '.result')
+API_TOKEN=$(curl -s -k -X POST -H 'Content-Type: application/json-rpc' -d '{"jsonrpc":"2.0", "method":"user.login", "params": {"user": "'$1'", "password": "'$2'"}, "auth": null, "id": 0}' ''$4'/api_jsonrpc.php' | jq -r '.result')
 
-GROUP_ID=$(curl -s -k -X POST -H 'Content-Type: application/json-rpc' -d '{"jsonrpc": "2.0", "method": "hostgroup.get", "params": {"output": "extend", "filter": {"name": ["'$3'"]}}, "auth": "'$API_TOKEN'", "id": 1}' ''$ZABBIX_URL'/api_jsonrpc.php' | jq '.result[].groupid' | sed 's/"//g')
+GROUP_ID=$(curl -s -k -X POST -H 'Content-Type: application/json-rpc' -d '{"jsonrpc": "2.0", "method": "hostgroup.get", "params": {"output": "extend", "filter": {"name": ["'$3'"]}}, "auth": "'$API_TOKEN'", "id": 1}' ''$4'/api_jsonrpc.php' | jq '.result[].groupid' | sed 's/"//g')
 
 echo $API_TOKEN
 echo $GROUP_ID
@@ -24,6 +22,6 @@ do
 	echo $HOST_NAME 
 	#echo $VISIBLE_NAME 
 	echo $IP_ADDR 
-	curl -s -k -X POST -H 'Content-Type: application/json-rpc' -d '{"jsonrpc": "2.0", "method": "host.create", "params": { "host": "'$HOST_NAME'", "name": "'$HOST_NAME'", "interfaces": [ { "type": 1, "main": 1, "useip": 1, "ip": "'$IP_ADDR'", "dns": "", "port": "10050" } ], "groups": [ { "groupid": "'$GROUP_ID'" } ] }, "auth": "'$API_TOKEN'", "id": 1}' ''$ZABBIX_URL'/api_jsonrpc.php' ; echo
+	curl -s -k -X POST -H 'Content-Type: application/json-rpc' -d '{"jsonrpc": "2.0", "method": "host.create", "params": { "host": "'$HOST_NAME'", "name": "'$HOST_NAME'", "interfaces": [ { "type": 1, "main": 1, "useip": 1, "ip": "'$IP_ADDR'", "dns": "", "port": "10050" } ], "groups": [ { "groupid": "'$GROUP_ID'" } ] }, "auth": "'$API_TOKEN'", "id": 1}' ''$4'/api_jsonrpc.php' ; echo
 	echo "---"
 done
